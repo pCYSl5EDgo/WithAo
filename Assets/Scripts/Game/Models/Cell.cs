7fa7976acaf3752396c037f1af8e89554e12a418
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-15 08:06:28 +0900

    [update]盤面と勢力実装

--- Assets/Scripts/Game/Models/Cell.cs
+++ Assets/Scripts/Game/Models/Cell.cs
@@ -1,10 +1,69 @@
-﻿namespace AoAndSugi.Game.Models
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using UniNativeLinq;
+using Unity.Mathematics;
+
+namespace AoAndSugi.Game.Models
 {
     public unsafe struct Cell
     {
         public CellType CellTypeValue;
-        public int TeamFlags;
+
+        /// <summary>
+        /// bitフラッグ管理
+        /// </summary>
+        public uint TeamFlags;
+
+        /// <summary>
+        /// 各チームの色塗り率
+        /// 0~255だが、bitフラッグがonなら1~256 offなら0と計算する
+        /// </summary>
         public fixed byte TeamValues[20];
+
+        public bool IsTerritoryOf(int teamId)
+        {
+            return ((TeamFlags >> teamId) & 0x1U) == 0x1U;
+        }
+
+        private void SetTerritoryOf(int teamId)
+        {
+            TeamFlags |= 1U << teamId;
+        }
+
+        public int Paint(int teamId, int value)
+        {
+            if (value <= 0) return 0;
+            var territoryCount = math.lzcnt(TeamFlags);
+            var sum = territoryCount;
+            for (var i = 0; i < 20; i++)
+            {
+                sum += TeamValues[i];
+            }
+            var total = sum + value;
+            if (total > 256)
+            {
+                if (IsTerritoryOf(teamId))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                if (IsTerritoryOf(teamId))
+                {
+                    TeamValues[teamId] = (byte)(TeamValues[teamId] + value);
+                }
+                else
+                {
+                    SetTerritoryOf(teamId);
+                    TeamValues[teamId] = (byte)(value - 1);
+                }
+            }
+        }
     }
 
     public enum CellType
---
+++ Assets/Scripts/Game/Models/Unit/UnitMovePower.cs
@@ -0,0 +1,7 @@
+﻿namespace AoAndSugi.Game.Models.Unit
+{
+    public struct UnitMovePower
+    {
+        public int Value;
+    }
+}
