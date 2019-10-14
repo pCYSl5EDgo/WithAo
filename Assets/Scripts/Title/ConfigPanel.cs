b4ce3e5688e7aabe001a4b51df81fbaffb61542e
ao <aochaaaaan.02@gmail.com>
2019-10-14 13:05:29 +0900

    [fix]設定が外れていたので修正

--- Assets/Scripts/Title/ConfigPanel.cs
+++ Assets/Scripts/Title/ConfigPanel.cs
@@ -5,8 +5,15 @@ public sealed class ConfigPanel : MonoBehaviour
 {
     [SerializeField] Slider slider;
 
+    private void OnEnable()
+    {
+        Debug.Log($"元の音量：{SoundManager.Instance.BgmVolume}");
+        slider.value = SoundManager.Instance.BgmVolume;
+    }
+
     public void OnValueChanged()
     {
+        Debug.Log($"変更：{slider.value}");
         SoundManager.Instance.BgmVolume = slider.value;
     }
 
