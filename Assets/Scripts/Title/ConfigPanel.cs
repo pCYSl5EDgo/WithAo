968b768581a333d4831fdcf3c65cb510715e8e1f
ao <aochaaaaan.02@gmail.com>
2019-10-14 13:07:35 +0900

    [fix]余分なコメントを削除

--- Assets/Scripts/Title/ConfigPanel.cs
+++ Assets/Scripts/Title/ConfigPanel.cs
@@ -7,13 +7,11 @@ public sealed class ConfigPanel : MonoBehaviour
 
     private void OnEnable()
     {
-        Debug.Log($"元の音量：{SoundManager.Instance.BgmVolume}");
         slider.value = SoundManager.Instance.BgmVolume;
     }
 
     public void OnValueChanged()
     {
-        Debug.Log($"変更：{slider.value}");
         SoundManager.Instance.BgmVolume = slider.value;
     }
 
