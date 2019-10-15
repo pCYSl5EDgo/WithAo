ff67c48d9fa252f2235b6edb476cec4ee818ff4b
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-15 16:31:45 +0900

    [add]Cell Test

---
+++ Assets/Scripts/Game/Game Tests/CellTests.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace AoAndSugi.Game.Models
+{
+    public class CellTests
+    {
+        [Test]
+        public void CellTestsSimplePasses()
+        {
+            var cell = new Cell();
+            Assert.IsTrue(@"{""ChipType"": ""None"", ""Territories"": []}" == cell.ToString());
+        }
+
+        [Test]
+        public void DefaultNoTerritory()
+        {
+            var cell = new Cell();
+            for (var i = 0; i < 20; i++)
+            {
+                Assert.IsFalse(cell.IsTerritoryOf(i));
+                Assert.IsTrue(0 == cell[i]);
+            }
+        }
+
+        [Test]
+        public void SimpleSetTest()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                var cell = new Cell();
+                cell[i] = 0;
+                Assert.IsFalse(cell.IsTerritoryOf(i));
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    Assert.IsFalse(cell.IsTerritoryOf(j));
+                }
+                for (var j = 1; j <= 256; j++)
+                {
+                    cell[i] = j;
+                    Assert.IsTrue(cell.IsTerritoryOf(i));
+                    Assert.IsTrue(j == cell[i]);
+                }
+            }
+        }
+
+        [Test]
+        public void SimpleSetOutOfRangeTest()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var cell = new Cell();
+                    cell[i] = -1;
+                });
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var cell = new Cell();
+                    cell[i] = 257;
+                });
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var cell = new Cell();
+                    cell[i] = int.MinValue;
+                });
+                Assert.Throws<ArgumentOutOfRangeException>(() =>
+                {
+                    var cell = new Cell();
+                    cell[i] = int.MaxValue;
+                });
+            }
+        }
+
+        [Test]
+        public void SimpleAddTest()
+        {
+            var cell = new Cell();
+            for (var i = 0; i < 20; i++)
+            {
+                cell.AddPaint(i, 1);
+                Assert.IsTrue(cell.IsTerritoryOf(i));
+                Assert.AreEqual(1, cell[i]);
+            }
+            Assert.AreEqual(20, cell.Sum());
+        }
+
+        [Test]
+        public void Two256Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    Cell cell = default;
+                    cell.AddPaint(i, 256);
+                    Assert.AreEqual(256 , cell[i]);
+                    cell.AddPaint(j, 256);
+                    Assert.AreEqual(0, cell[i]);
+                    Assert.AreEqual(256, cell[j]);
+                }
+            }
+        }
+
+        [Test]
+        public void Two1024Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    Cell cell = default;
+                    cell.AddPaint(i, 1024);
+                    Assert.AreEqual(256, cell[i]);
+                    cell.AddPaint(j, 1024);
+                    Assert.AreEqual(0, cell[i]);
+                    Assert.AreEqual(256, cell[j]);
+                }
+            }
+        }
+
+        [Test]
+        public void Three128Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    for (var k = 0; k < 20; k++)
+                    {
+                        if (i == k || j == k) continue;
+                        Cell cell = default;
+                        cell.AddPaint(i, 128);
+                        Assert.IsTrue(128 == cell[i]);
+                        cell.AddPaint(j, 128);
+                        Assert.AreEqual(128, cell[i]);
+                        Assert.AreEqual(128, cell[j]);
+                        Assert.AreEqual(256, cell.Sum());
+                        cell.AddPaint(k, 128);
+                        Assert.IsTrue(64 == cell[i]);
+                        Assert.IsTrue(64 == cell[j]);
+                        Assert.IsTrue(128 == cell[k]);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void Three256Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    for (var k = 0; k < 20; k++)
+                    {
+                        if (i == k || j == k) continue;
+                        Cell cell = default;
+                        cell.AddPaint(i, 256);
+                        Assert.IsTrue(256 == cell[i]);
+                        cell.AddPaint(j, 256);
+                        Assert.IsTrue(0 == cell[i]);
+                        Assert.IsTrue(256 == cell[j]);
+                        cell.AddPaint(k, 256);
+                        Assert.IsTrue(0 == cell[i]);
+                        Assert.IsTrue(0 == cell[j]);
+                        Assert.IsTrue(256 == cell[k]);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void Three1024Test()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                for (var j = 0; j < 20; j++)
+                {
+                    if (i == j) continue;
+                    for (var k = 0; k < 20; k++)
+                    {
+                        if (i == k || j == k) continue;
+                        Cell cell = default;
+                        cell.AddPaint(i, 1024);
+                        Assert.IsTrue(256 == cell[i]);
+                        cell.AddPaint(j, 1024);
+                        Assert.IsTrue(0 == cell[i]);
+                        Assert.IsTrue(256 == cell[j]);
+                        cell.AddPaint(k, 1024);
+                        Assert.IsTrue(0 == cell[i]);
+                        Assert.IsTrue(0 == cell[j]);
+                        Assert.IsTrue(256 == cell[k]);
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void Twenty13Test()
+        {
+            Cell cell = default;
+            for (var i = 0; i < 20; i++)
+            {
+                cell.AddPaint(i, 13);
+                Assert.AreEqual(13, cell[i]);
+            }
+            for (var i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(12, cell[i]);
+            }
+            for (var i = 4; i < 20; i++)
+            {
+                Assert.AreEqual(13, cell[i]);
+            }
+        }
+
+        [Test]
+        public void ComplexTest1()
+        {
+            Cell cell = default;
+            cell.AddPaint(0, 76);
+            Assert.AreEqual(76, cell[0]);
+            cell.AddPaint(1, 68);
+            Assert.AreEqual(144, cell.Sum());
+            Assert.AreEqual(68, cell[1]);
+            cell.AddPaint(2, 53);
+            Assert.AreEqual(197, cell.Sum());
+            Assert.AreEqual(53, cell[2]);
+            cell.AddPaint(3, 100); // 297 - 256 = 41
+            // 41 = 13 * 3 + 2
+            Assert.AreEqual(256, cell.Sum());
+            Assert.AreEqual(100, cell[3]);
+            Assert.AreEqual(76 - 14, cell[0]);
+            Assert.AreEqual(68 - 14, cell[1]);
+            Assert.AreEqual(53 - 13, cell[2]);
+        }
+
+        [Test]
+        public void ComplexTest2()
+        {
+            Cell cell = default;
+            cell.AddPaint(0, 76);
+            cell.AddPaint(1, 68);
+            cell.AddPaint(2, 53);
+            cell.AddPaint(3, 10);
+            cell.AddPaint(4, 190); // 397 - 256 = 141 141 = 4 * 35 + 1
+            
+            Assert.AreEqual(256, cell.Sum());
+            Assert.AreEqual(190, cell[4]);
+            // 141 - 36 - 35*2 - 10 = 61 - 36 = 25 = 8 * 3 + 1
+            Assert.AreEqual(76 - 36 - 9, cell[0]);
+            Assert.AreEqual(68 - 35 - 8, cell[1]);
+            Assert.AreEqual(53 - 35 - 8, cell[2]);
+            Assert.IsFalse(cell.IsTerritoryOf(3));
+            Assert.AreEqual(190, cell[4]);
+        }
+    }
+}
--- Assets/Scripts/Game/Models/Cell.cs
+++ Assets/Scripts/Game/Models/Cell.cs
@@ -1,6 +1,6 @@
-﻿using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using UniNativeLinq;
+﻿using System;
+using System.Text;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
 
 namespace AoAndSugi.Game.Models
@@ -20,49 +20,167 @@ namespace AoAndSugi.Game.Models
         /// </summary>
         public fixed byte TeamValues[20];
 
-        public bool IsTerritoryOf(int teamId)
-        {
-            return ((TeamFlags >> teamId) & 0x1U) == 0x1U;
-        }
+        public bool IsTerritoryOf(int teamId) => ((TeamFlags >> teamId) & 0x1U) != 0;
 
-        private void SetTerritoryOf(int teamId)
+        public int Sum()
         {
-            TeamFlags |= 1U << teamId;
-        }
-
-        public int Paint(int teamId, int value)
-        {
-            if (value <= 0) return 0;
-            var territoryCount = math.lzcnt(TeamFlags);
-            var sum = territoryCount;
+            var answer = math.countbits(TeamFlags);
             for (var i = 0; i < 20; i++)
             {
-                sum += TeamValues[i];
+                answer += TeamValues[i];
             }
-            var total = sum + value;
-            if (total > 256)
-            {
-                if (IsTerritoryOf(teamId))
-                {
+            return answer;
+        }
 
+        public int this[int teamId]
+        {
+            get => IsTerritoryOf(teamId) ? TeamValues[teamId] + 1 : 0;
+            set
+            {
+                #if UNITY_EDITOR
+                if (value < 0) throw new ArgumentOutOfRangeException(value + " should not be less than 0!");
+                else if (value > 256) throw new ArgumentOutOfRangeException(value + " should not be greater than 256!");
+                #endif
+                if (value == 0)
+                {
+                    TeamValues[teamId] = 0;
+                    TeamFlags &= ~(1U << teamId);
                 }
                 else
                 {
-
+                    TeamValues[teamId] = (byte) (value - 1);
+                    TeamFlags |= 1U << teamId;
                 }
             }
-            else
+        }
+
+        public int AddPaint(int teamId, int value)
+        {
+            #if UNITY_EDITOR
+            if (value < 0) throw new ArgumentOutOfRangeException(value + " should not be less than 0!");
+            #endif
+            if (value == 0) return 0;
+            var territoryCount = math.countbits(TeamFlags);
+
+            if (value >= 256)
             {
-                if (IsTerritoryOf(teamId))
+                Clear();
+                this[teamId] = 256;
+                return 256;
+            }
+
+            // tzcnt ２の累乗なら何乗なのかわかる
+
+            switch (territoryCount)
+            {
+            case 0:
+                return this[teamId] = value;
+            case 1:
+                var targetTeamId = math.tzcnt(TeamFlags);
+                if (targetTeamId == 64)
+                    targetTeamId = 0;
+                if (targetTeamId == teamId)
                 {
-                    TeamValues[teamId] = (byte)(TeamValues[teamId] + value);
+                    var old = this[teamId];
+                    var renew = old + value;
+                    if (renew > 256)
+                        renew = 256;
+                    this[teamId] = renew;
+                    return renew;
                 }
                 else
                 {
-                    SetTerritoryOf(teamId);
-                    TeamValues[teamId] = (byte)(value - 1);
+                    this[teamId] = value;
+                    var sum = this[targetTeamId] + value;
+                    if (sum <= 256)
+                    {
+                        return value;
+                    }
+                    this[targetTeamId] = 256 - value;
+                    return value;
+                }
+            default:
+                {
+                    var old = this[teamId];
+                    if (old + value >= 256)
+                    {
+                        Clear();
+                        this[teamId] = 256;
+                        return 256;
+                    }
+                    var sum = Sum();
+                    if (sum + value <= 256)
+                    {
+                        this[teamId] = old + value;
+                    }
+                    else
+                    {
+                        Decrease(exceptId: teamId, exceptIdValue: old + value, overflow: sum + value - 256);
+                    }
+                    return old + value;
                 }
             }
+        }
+
+        private void Decrease(int exceptId, int exceptIdValue, int overflow)
+        {
+            #if UNITY_EDITOR
+            if (exceptId < 0 || exceptId > 20) throw new ArgumentOutOfRangeException(exceptId.ToString());
+            if (exceptIdValue < 0 || exceptIdValue > 256) throw new ArgumentOutOfRangeException(exceptIdValue.ToString());
+            if (overflow <= 0) throw new ArgumentOutOfRangeException(overflow.ToString());
+            #endif
+            this[exceptId] = 0;
+
+            do
+            {
+                var count = math.countbits(TeamFlags);
+                var dec = overflow / count;
+                var rest = overflow - dec * count;
+                for (var i = 0; i < 20; i++)
+                {
+                    var val = this[i];
+                    if (val == 0) continue;
+                    if (rest > 0)
+                    {
+                        rest--;
+                        val--;
+                    }
+                    if (dec <= val)
+                    {
+                        this[i] = val - dec;
+                        continue;
+                    }
+                    this[i] = 0;
+                    rest += dec - val;
+                }
+                overflow = rest;
+            } while (overflow != 0);
+
+            this[exceptId] = exceptIdValue;
+        }
+
+        public void Clear()
+        {
+            fixed (void* ptr = TeamValues)
+            {
+                UnsafeUtility.MemClear(ptr, 20L);
+            }
+            TeamFlags = 0;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append("{\"ChipType\": \"").Append(CellTypeValue.ToString()).Append("\", \"Territories\": [");
+            for (var i = 0; i < 20; i++)
+            {
+                if (IsTerritoryOf(i))
+                {
+                    builder.Append("\n\t{\"Team\": ").Append(i).Append(", \"Value\": ").Append(this[i]).Append("},");
+                }
+            }
+            builder.Append("]}");
+            return builder.ToString();
         }
     }
 
