e140da78443c2cb3e2447778b7db81759bf2e95a
ao <aochaaaaan.02@gmail.com>
2019-10-19 07:30:58 +0900

    [update]情報をリストに反映させる

--- Assets/Scripts/Title/CurrentRoomIcon.cs
+++ Assets/Scripts/Title/CurrentRoomIcon.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 using Zenject;
+using AoAndSugi.Game.Models;
+using Unity.Mathematics;
 
 namespace AoAndSugi
 {
@@ -15,8 +17,10 @@ namespace AoAndSugi
 
         [SerializeField] RectTransform rectTransform;
         [SerializeField] TextMeshProUGUI roomName;
-        [SerializeField] TextMeshProUGUI count;
-
+        [SerializeField] TextMeshProUGUI mapSize;
+        [SerializeField] TextMeshProUGUI playerCount;
+        [SerializeField] TextMeshProUGUI npcCount;
+        
         public void OnClickButton() {
             PhotonNetwork.JoinRoom(roomName.text);
             waitPanel.gameObject.SetActive(true);
@@ -25,7 +29,20 @@ namespace AoAndSugi
         public void Activate(RoomInfo info)
         {
             roomName.text = info.Name;
-            count.text = $"{info.PlayerCount}/{info.MaxPlayers}";
+            playerCount.text = $"{info.PlayerCount}/{info.MaxPlayers}";
+
+            object value = null;
+            if (info.CustomProperties.TryGetValue("BordSize", out value))
+            {
+                int2 size = ((BoardSize)value).Value;
+                mapSize.text = $"{ size.x }*{ size.y }";
+            }
+            value = null;
+            if (info.CustomProperties.TryGetValue("NpcCount", out value))
+            {
+                int count = ((MaxTeamCount)value).Value;
+                npcCount.text = $"{ count }";
+            }
         }
 
         public void Deactivate()
